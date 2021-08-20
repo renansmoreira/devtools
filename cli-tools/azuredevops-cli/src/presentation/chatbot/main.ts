3b0729b1d85bc065ee3d539bb0d4f3e2063b49a1
Renan S Moreira <renan.siravegna@gmail.com>
2021-08-20 17:26:19 -0400

    Configuring bot preffix to env var

--- cli-tools/azuredevops-cli/src/presentation/chatbot/main.ts
+++ cli-tools/azuredevops-cli/src/presentation/chatbot/main.ts
@@ -1,5 +1,6 @@
 import { startBot, sendMessage } from 'https://deno.land/x/discordeno/mod.ts';
 import { AzureDevOpsHttpClient } from '../../infra/azureDevOpsHttpClient.ts';
+import { ConfigJsonProvider } from '../../configs/configJsonProvider.ts';
 import { ExecutePipelineService } from '../../services/executePipelineService.ts';
 import { MessageParser } from './messageParser.ts';
 import { MessageCommand } from './messagecommands/messageCommand.ts';
@@ -7,9 +8,10 @@ import { PipelineMessageCommand } from './messagecommands/pipelineMessageCommand
 import { DiscordMessage } from './discordMessage.ts';
 
 const azureDevOpsClient = new AzureDevOpsHttpClient();
+const configProvider = new ConfigJsonProvider();
 
-const messageParser = new MessageParser('boga', [
-  new PipelineMessageCommand(new ExecutePipelineService(azureDevOpsClient))
+const messageParser = new MessageParser(Deno.env.get('DISCORD_BOT_PREFFIX'), [
+  new PipelineMessageCommand(new ExecutePipelineService(azureDevOpsClient, configProvider), configProvider)
 ]);
 
 startBot({
@@ -20,6 +22,7 @@ startBot({
       console.log('Successfully connected to gateway');
     },
     async messageCreate(message) {
+      console.log(message);
       const messageCommands = messageParser.parse(message) || [];
 
       for (let command of messageCommands) {
